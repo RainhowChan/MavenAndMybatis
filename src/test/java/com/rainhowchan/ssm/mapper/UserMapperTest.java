9b97ea6547de9da94dea1ce7281cd325fa21db53
RainhowChan <rainhowchan@163.com>
2017-01-06 01:47:35 +0800

    first commit

---
+++ src/test/java/com/rainhowchan/ssm/mapper/UserMapperTest.java
@@ -0,0 +1,29 @@
+package com.rainhowchan.ssm.mapper;
+
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by RainhowChan on 2017/1/6.
+ */
+public class UserMapperTest {
+
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+    public void selectByExample() throws Exception {
+
+    }
+
+    @Test
+    public void selectByPrimaryKey() throws Exception {
+
+    }
+
+}
