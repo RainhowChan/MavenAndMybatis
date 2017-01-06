195b430e392e7e381280ebfee4555b0bafb6451c
ZhangHanyi <rainhowchan@163.com>
2017-01-06 16:29:57 +0800

    mybatis test success

--- src/test/java/com/rainhowchan/ssm/mapper/UserMapperTest.java
+++ src/test/java/com/rainhowchan/ssm/mapper/UserMapperTest.java
@@ -1,29 +1,48 @@
 package com.rainhowchan.ssm.mapper;
 
+import com.rainhowchan.ssm.po.User;
+import com.rainhowchan.ssm.po.UserExample;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by RainhowChan on 2017/1/6.
  */
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext-dao.xml")*/
 public class UserMapperTest {
 
+    private UserMapper userMapper;
     @Before
-    public void init(){
-
+    public void init() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-dao.xml");
+        userMapper= (UserMapper) context.getBean("userMapper");
     }
 
     @Test
     public void selectByExample() throws Exception {
-
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andUsernameLike("%王%");
+        List<User> users = userMapper.selectByExample(userExample);
+        for (User user : users) {
+            System.out.println(user.getAddress());
+        }
     }
 
     @Test
     public void selectByPrimaryKey() throws Exception {
-
+        User user=userMapper.selectByPrimaryKey(10);
+        System.out.println(user.getUsername());
     }
 
 }
